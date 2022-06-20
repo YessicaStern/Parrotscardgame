460c9a34bb36d72776e4cbfcac4074e6bc3ee813
YessicaStern <yessica_stern@hotmail.com>
2022-06-20 01:28:01 -0300

    p4save3: cartas pares, settimeout, a rotacao deu ruim

--- projeto4-parrotscardgame/script.js
+++ projeto4-parrotscardgame/script.js
@@ -1,40 +1,60 @@
 let quantidade = prompt("escolha de 4-14 cartas");
-let arraycartaspares=[];
-let puxarcartas;
-let arrayimagens=[ "bobross","explody","fiesta","metal","revertit","triplets", "unicorn",];
+let cartaspares=[];
+let imagens=[ "bobross", "bobross","explody","explody","fiesta","fiesta","metal","metal","revertit","revertit","triplets", "triplets","unicorn", "unicorn"];
+let ctotal=0;
+let c=0;
+let imgsrc1;
+let imgsrc2;
 
 quantidade=Number(quantidade);
 let resto=quantidade%2;
 
-
 if(quantidade>= 4 && quantidade <=14 && resto===0){
-}
-while(quantidade>14 || quantidade<4 ||  isNaN(quantidade)==true || resto===1 || quantidade==null){
+}while(quantidade>14 || quantidade<4 ||  isNaN(quantidade)==true || resto===1 || quantidade==null){
   quantidade = prompt("Comando inválido, Escolha um número par entre 4-14");
   resto=quantidade%2;
-}
-alert(`muito bem, ${quantidade} cartas`);
+}alert(`muito bem, ${quantidade} cartas`);
 
+function comparador() { 
+	return Math.random() - 0.5; }
 
 criarcartas();
+
 function criarcartas(){
   for(let i=0;i<quantidade;i++){
-    let x=0
-    q=quantidade/2  
-  let cartastemplate= `<div class="box" onclick="selecao(this)">
-                      <img src="/projeto4-parrotscardgame/imagens/front.png">
-                      <img src="imagens/unicornparrot.gif" class="imgverso esconder"></div>`
-                      
-  document.querySelector(".corpo").innerHTML+=cartastemplate;
-/*  arraycartaspares.push(puxarcartas);
-  arraycartaspares.push(puxarcartas);*/
+    cartaspares.push(imagens[i]);
+  }cartaspares.sort(comparador);
+  for(let i=0;i<quantidade;i++){
+    let cartastemplate= `<div class="box" onclick="selecao(this)"><img class="frente" src="/projeto4-parrotscardgame/imagens/front.png">
+                         <img src="imagens/${cartaspares[i]}parrot.gif" class="verso esconder"></div>`
+    document.querySelector(".corpo").innerHTML+=cartastemplate;
 }}
 
-/*box.sort(comparador);
-function comparador() { 
-	return Math.random() - 0.5; 
-}*/
-
 function selecao(elemento){
-  elemento.classList.toggle("verso");
-}
+let verso = elemento.querySelector(".verso");
+let frente = elemento.querySelector(".frente");
+let imgsrc= elemento.querySelector(".verso");
+  c++;
+  if(c<3){
+  verso.classList.remove("esconder");
+  frente.classList.add("esconder");
+  console.log(c);}
+  if(c===1){
+    imgsrc1=imgsrc; 
+  }if(c===2){
+    imgsrc2=imgsrc;
+  }if(imgsrc1.src===imgsrc2.src){
+    console.log("é igual");
+    c=0;
+  }if (c===2 && imgsrc1 !== imgsrc2){ 
+    setTimeout(function voltarcartas(){
+                let img1=imgsrc1.parentNode;
+                let img2=imgsrc2.parentNode;
+                imgsrc1.classList.add("esconder");
+                img1.querySelector(".frente").classList.remove("esconder");
+                imgsrc2.classList.add("esconder");
+                img2.querySelector(".frente").classList.remove("esconder");
+    },1000);
+    console.log("n é igual");
+    c=0;
+  }}
